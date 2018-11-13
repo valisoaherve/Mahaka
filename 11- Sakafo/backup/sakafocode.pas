3c584af975fcb1a14c7a3deb76cbc831f61f863f
valisoaherve <40111187+valisoaherve@users.noreply.github.com>
2018-11-13 12:04:22 +0300

    Add files via upload

---
+++ 11- Sakafo/backup/sakafocode.pas
@@ -0,0 +1,48 @@
+unit sakafocode;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Grids,
+  StdCtrls, ExtCtrls;
+
+type
+
+  { TSakafoForm }
+
+  TSakafoForm = class(TForm)
+    Daty: TStaticText;
+    sary: TImage;
+    SakafoMenu: TEdit;
+    StringGrid1: TStringGrid;
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  SakafoForm: TSakafoForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TSakafoForm }
+
+procedure TSakafoForm.FormCreate(Sender: TObject);
+begin
+// sary.Picture.LoadFromFile('images/Mardi.jpg');
+end;
+
+procedure TSakafoForm.FormActivate(Sender: TObject);
+begin
+ Daty.Caption:=GetFormatDatetime('dddd',Now);
+end;
+
+end.
+
