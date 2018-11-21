d66d406ed9e8771acffa9b47bf17faf7019bde30
Valisoa Jean Hervé <valisoaherve@gmail.com>
2018-11-21 11:49:28 +0300

    screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 'use strict' 
 importScripts('sw-toolbox.js')
-toolbox.precache(['index.html','style.min.css'])
+toolbox.precache(['index.html','style.min.css','partage/partage.html','partage/stylepartage.css','partage/logo.jpg'])
 // toolbox.router.get('/images/*', toolbox.cacheFirst)
 toolbox.router.get('/*',toolbox.networkFirst,{networkTimeoutSeconds:5} )
